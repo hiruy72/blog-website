44436bf1672a4e6119f53b45bc99b62887c17625
Hiruy Legesse <hiruyadane@gmail.com>
2025-11-17 23:50:06 -0800

    Setup auth system

---
+++ app/(auth)/sign-up/page.tsx
@@ -0,0 +1,9 @@
+import SignUpForm from "@/components/signup-form";
+
+export default function SignUpPage(){
+  return (
+    <div className="flex items-center justify-center w-full h-dvh">
+       <SignUpForm/>
+    </div>
+  )
+}
--- lib/auth.ts
+++ lib/auth.ts
@@ -2,6 +2,7 @@ import { betterAuth } from "better-auth";
 import { prismaAdapter } from "better-auth/adapters/prisma";
 // If your Prisma file is located elsewhere, you can change the path
 import { PrismaClient } from "@/lib/generated/prisma/client";
+import { nextCookies } from "better-auth/next-js";
 
 const prisma = new PrismaClient();
 export const auth = betterAuth({
@@ -10,5 +11,19 @@ export const auth = betterAuth({
     }),
     emailAndPassword:{
         enabled:true,
-    }
+    },
+    socialProviders: {
+        github: {
+            clientId: process.env.GITHUB_CLIENT_ID!,
+            clientSecret: process.env.GITHUB_CLIENT_SECRET!,
+            prompt: "select_account"
+
+        },
+        google:{
+            clientId: process.env.GOOGLE_CLIENT_ID!,
+            clientSecret: process.env.GOOGLE_CLIENT_SECRET!,
+            prompt: "select_account"
+        }
+    },
+    plugins: [ nextCookies()],
 });
