c204016702c034d54a705ffaae12345a889016f7
Hiruy Legesse <hiruyadane@gmail.com>
2025-11-21 23:49:22 -0800

    Add dashboard

---
+++ app/(admin)/layout.tsx
@@ -0,0 +1,17 @@
+import { AppSidebar } from "@/components/app-sidebar"
+import { SidebarProvider, SidebarTrigger } from "@/components/ui/sidebar"
+
+
+export default function AdminLayout({children}: {children: React.ReactNode}) {
+    return(
+
+    <SidebarProvider>
+      <AppSidebar />
+      <main>
+        <SidebarTrigger />
+        {children}
+      </main>
+    </SidebarProvider>
+
+    )
+}
